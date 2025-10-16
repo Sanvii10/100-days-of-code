feca5967f09102ceccb5c713e565c742a39c82d4
Sanvi <sanvibawa10@gmail.com>
2025-10-16 21:54:55 +0530

    Initial commit

--- day14.c
+++ day14.c
@@ -47,4 +47,23 @@ Input 2:
 Output 2:
 48 (2 * 4 * 6)
 
+
+
+#include <stdio.h>
+
+int main() {
+    int n, i, product = 1;
+    
+    scanf("%d", &n);
+    
+    
+    for (i = 2; i <= n; i = i + 2) {
+        product = product * i;
+    }
+    
+    printf("%d\n", product);
+    
+    return 0;
+}
+
 */
