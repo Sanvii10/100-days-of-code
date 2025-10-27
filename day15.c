a24db43c3266c9af4193cab935fb283a43747b46
Sanvi <sanvibawa10@gmail.com>
2025-10-27 21:56:27 +0530

    Initial commit

--- day15.c
+++ day15.c
@@ -55,5 +55,25 @@ Input 2:
 Output 2:
 1
 
-*/
 
+
+#include <stdio.h>
+
+int main() {
+    int n, reverse = 0, digit;
+    
+    scanf("%d", &n);
+    
+    // Reverse the number
+    while (n > 0) {
+        digit = n % 10;
+        reverse = reverse * 10 + digit;
+        n = n / 10;
+    }
+    
+    printf("%d\n", reverse);
+    
+    return 0;
+}
+
+*/
