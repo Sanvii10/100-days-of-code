8a897dc14039957dc8b979309e06b9373b78fa8b
Sanvi <sanvibawa10@gmail.com>
2025-11-05 22:29:18 +0530

    Initial commit

--- day23.c
+++ day23.c
@@ -1,5 +1,3 @@
-
-
 /*Q46: Write a program to print the following pattern:
 *****
 *****
@@ -34,3 +32,42 @@ int main() {
     return 0;
 }
     */
+
+/*Q45: Write a program to find the sum of the series: 2/3 + 4/7 + 6/11 + 8/15 + ... up to n terms.
+
+
+Sample Test Cases:
+Input 1:
+3
+Output 1:
+Approximate sum: 1.56
+
+Input 2:
+5
+Output 2:
+Approximate sum: 2.22
+
+
+
+
+#include <stdio.h>
+
+int main() {
+    int n, i, numerator, denominator;
+    float sum = 0.0;
+    
+    scanf("%d", &n);
+    
+    // Calculate sum of series
+    for (i = 1; i <= n; i++) {
+        numerator = 2 * i;           // 2, 4, 6, 8, ...
+        denominator = 4 * i - 1;     // 3, 7, 11, 15, ...
+        sum = sum + (float)numerator / denominator;
+    }
+    
+    printf("Approximate sum: %.2f\n", sum);
+    
+    return 0;
+}
+
+*/
