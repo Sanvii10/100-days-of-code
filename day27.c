caf36b2614f0fb55fde91431062b883091015699
Sanvi <sanvibawa10@gmail.com>
2025-11-12 22:33:32 +0530

    Initial commit

--- day27.c
+++ day27.c
@@ -23,6 +23,9 @@ Output 1:
 *****
 ***
 *
+
+
+
 #include <stdio.h>
 
 int main() {
@@ -66,11 +69,39 @@ Input 1:
 Output 1:
 Pattern with layers of stars as shown.
 
-*/
 
 #include <stdio.h>
 
 int main() {
+    int n = 4, i, j;
+    
+
+    for (i = 1; i <= n; i++) {
+        
+        for (j = 1; j <= n - i; j++)
+            printf(" ");
+        
+        
+        for (j = 1; j <= 2 * i - 1; j++)
+            printf("*");
+        
+        printf("\n");
+    }
+    
+
+    for (i = n - 1; i >= 1; i--) {
+        
+        for (j = 1; j <= n - i; j++)
+            printf(" ");
+        
+        
+        for (j = 1; j <= 2 * i - 1; j++)
+            printf("*");
+        
+        printf("\n");
+    }
     
     return 0;
 }
+
+*/
